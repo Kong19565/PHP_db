0fd277ce5cfdad65873f8fca895335191dd1de15
Kong19565 <khaogong48@gmail.com>
2023-07-05 11:23:15 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "kong19565";
+$dbname = "db_killer";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+?>
